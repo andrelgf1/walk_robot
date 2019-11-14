7139553e535c77dbcf89e44a1d9e730e573ee718
Andre Ferreira <andrelgferreira1@gmail.com>
2019-11-14 15:48:16 -0500

    Added walkRobot.cpp. File for the implementation of the methods of the walkRobot class and for the implementation of the contructor

---
+++ src/walkRobot.cpp
@@ -0,0 +1,52 @@
+/**
+ * Distributed under the BSD License (license terms found in LICENSE or at https://opensource.org/licenses/BSD-2-Clause)
+ *
+ * @file walkRobot.cpp
+ *
+ * @brief Header implementation of methods of the walkRobot class
+ *
+ * @author Andre Ferreira
+ *
+ * @copyright  Andre Ferreira
+ *
+ */
+
+#include <stdlib.h>
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include "walk_robot/walkRobot.hpp"
+
+walkRobot::walkRobot() {
+/// initializing obstacle variable to false
+  obstacle = false;
+  avoided = 0;
+}
+
+void walkRobot::laserSensorCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
+/// checking if laser found any obstacle
+  for (auto i : msg->ranges) {
+    if (i < 1) {
+      obstacle = true;
+      return;
+    }
+  }
+/// if no obstacle was found set obstacle to false
+  obstacle = false;
+}
+
+bool walkRobot:: collisionCheck() {
+  return obstacle;
+}
+
+void walkRobot::setAvoided(int number) {
+/// set number to variable avoided
+  avoided = number;
+  ROS_INFO_STREAM("Number of obstacles avoided"<< avoided);
+}
+
+int walkRobot::getAvoided() {
+  return avoided;
+}
+
+
+
